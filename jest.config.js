fa4aacc3968c241def50d1ea0b13d450cfb008f6
Emanuel Rabina <emanuelrabina@gmail.com>
2021-10-06 12:38:51 +1300

    Update dependencies - should fix the build missing something

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,12 @@
 /* eslint-env node */
 'use strict'; // eslint-disable-line
 
-/**
- * Jest configuration.
- */
 module.exports = {
 	collectCoverage: true,
 	coverageReporters: [
 		'html',
 		'lcov',
 		'text-summary'
-	]
+	],
+	testEnvironment: 'jsdom'
 };
